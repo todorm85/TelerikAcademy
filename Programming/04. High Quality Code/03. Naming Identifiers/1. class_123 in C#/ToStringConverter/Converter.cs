807bb824c0c92aa99ed6ba4f724193be72f85d6e
todorm85 <todorm85@abv.bg>
2015-06-27 02:57:16 +0300

    Add Programming\04. High Quality Code\03. Naming Identifiers\

---
+++ Programming/04. High Quality Code/03. Naming Identifiers/1. class_123 in C#/ToStringConverter/Converter.cs
@@ -0,0 +1,13 @@
+﻿namespace ToStringConverter
+{
+    using System;
+
+    internal class Converter
+    {
+        internal string BoolToString(bool value)
+        {
+            string valueAsString = value.ToString();
+            return valueAsString;
+        }
+    }
+}
---
+++ Programming/04. High Quality Code/03. Naming Identifiers/1. class_123 in C#/ToStringConverter/EntryPoint.cs
@@ -0,0 +1,15 @@
+﻿namespace ToStringConverter
+{
+    using System;
+
+    internal class EntryPoint
+    {
+        public static void Main()
+        {
+            Converter converter = new Converter();
+            var trueBoolValueAsString = converter.BoolToString(true);
+            Console.WriteLine(trueBoolValueAsString);
+        }
+    }
+}
+
