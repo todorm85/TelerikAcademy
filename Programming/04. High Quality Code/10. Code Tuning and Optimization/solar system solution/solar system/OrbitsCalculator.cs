d70cbe04d7c14dd8ae668af07ab9079d2707ba0c
Todor Mitskovski <todorbm85@gmail.com>
2015-07-20 18:53:14 +0300

    Add solution

--- Programming/04. High Quality Code/10. Code Tuning and Optimization/solar system solution/solar system/OrbitsCalculator.cs
+++ Programming/04. High Quality Code/10. Code Tuning and Optimization/solar system solution/solar system/OrbitsCalculator.cs
@@ -93,10 +93,11 @@ namespace SolarSystem
 
         private void EarthRotation()
         {
-			for (decimal step = 0; step <= 360; step+=0.00005m)
-			{
-				EarthRotationAngle = ((double)step) * Days / EarthRotationPeriod;
-			}
+            for (decimal step = 0; step <= 360; step += 0.00005m)
+            {
+                EarthRotationAngle = ((double)step) * Days / EarthRotationPeriod;
+            }
+            //EarthRotationAngle = 360 * Days / EarthRotationPeriod;
             Update("EarthRotationAngle");
         }
 
