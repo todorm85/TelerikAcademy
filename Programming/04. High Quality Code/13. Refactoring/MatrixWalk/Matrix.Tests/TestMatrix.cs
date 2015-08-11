9ec24b97d001607198d9ef37c502d314db682be4
Todor Mitskovski <todorbm85@gmail.com>
2015-08-11 11:32:50 +0300

    Add new solutions

---
+++ Programming/04. High Quality Code/13. Refactoring/MatrixWalk/Matrix.Tests/TestMatrix.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Matrix;
+
+namespace Matrix.Tests
+{
+    [TestClass]
+    public class TestMatrix
+    {
+        [TestMethod]
+        public void MatrixWalkShouldWorkWithValidInput()
+        {
+            var expected = "Enter a positive number \r\n" +
+                                "    1   19   20   21   22   23   24\r\n" +
+                                "   18    2   33   34   35   36   25\r\n" +
+                                "   17   40    3   32   39   37   26\r\n" +
+                                "   16   48   41    4   31   38   27\r\n" +
+                                "   15   47   49   42    5   30   28\r\n" +
+                                "   14   46   45   44   43    6   29\r\n" +
+                                "   13   12   11   10    9    8    7\r\n";
+
+            using (StringReader input = new StringReader("7"))
+            {
+                Console.SetIn(input);
+                using (StringWriter output = new StringWriter())
+                {
+                    Console.SetOut(output);                    
+                   //run Methods for testing.....
+                    MatrixWalk.Main();
+
+                    Assert.AreEqual(expected, output.ToString());
+                }
+            }
+        }
+
+        [TestMethod]
+        public void MatrixWalkShouldNotAcceptInvalidInput()
+        {
+            var expected = "Enter a positive number \r\n" +
+                            "You haven't entered a correct positive number\r\n" +
+                            "You haven't entered a correct positive number\r\n";
+
+            using (StringReader input = new StringReader("-3\n101\n0\n"))
+            {
+                Console.SetIn(input);
+                using (StringWriter output = new StringWriter())
+                {
+                    Console.SetOut(output);                    
+                   //run Methods for testing.....
+                    MatrixWalk.Main();
+
+                    Assert.AreEqual(expected, output.ToString());
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ChangeDirectionClockwiseShouldSwitchDirectionCorrectly()
+        {
+            int currentDirRow = 0;
+            int currentDirCol = 1;
+
+            MatrixWalk.ChangeDirectionClockwise(ref currentDirRow, ref currentDirCol);
+            Assert.AreEqual(1, currentDirRow);
+            Assert.AreEqual(1, currentDirCol);
+
+            MatrixWalk.ChangeDirectionClockwise(ref currentDirRow, ref currentDirCol);
+            Assert.AreEqual(1, currentDirRow);
+            Assert.AreEqual(0, currentDirCol);
+        }
+    }
+}
---
+++ Programming/04. High Quality Code/13. Refactoring/MatrixWalk/Matrix/MatrixWalk.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace Matrix
+{
+    public class MatrixWalk
+    {
+        public static void ChangeDirectionClockwise(ref int currentDirectionRow, ref int currentDirectionCol)
+        {
+            int[] rowDirections = { 1, 1, 1, 0, -1, -1, -1, 0 };
+            int[] colDirections = { 1, 0, -1, -1, -1, 0, 1, 1 };
+
+            int currentDirectionIndex = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                if (rowDirections[i] == currentDirectionRow && colDirections[i] == currentDirectionCol)
+                {
+                    currentDirectionIndex = i;
+                    break;
+                }
+            }
+
+            if (currentDirectionIndex == 7)
+            {
+                currentDirectionIndex = -1;
+            }
+
+            currentDirectionRow = rowDirections[currentDirectionIndex + 1];
+            currentDirectionCol = colDirections[currentDirectionIndex + 1];
+        }
+
+        public static bool CheckIfFreeNeighbourCellExists(int[,] arr, int currentRow, int currentCol)
+        {
+            int[] rowDirections = { 1, 1, 1, 0, -1, -1, -1, 0 };
+            int[] colDirections = { 1, 0, -1, -1, -1, 0, 1, 1 };
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (currentRow + rowDirections[i] >= arr.GetLength(0) || currentRow + rowDirections[i] < 0)
+                {
+                    rowDirections[i] = 0;
+                }
+
+                if (currentCol + colDirections[i] >= arr.GetLength(0) || currentCol + colDirections[i] < 0)
+                {
+                    colDirections[i] = 0;
+                }
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (arr[currentRow + rowDirections[i], currentCol + colDirections[i]] == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool FindFirstFreeCell(int[,] arr, ref int row, ref int col)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] == 0)
+                    {
+                        row = i;
+                        col = j;
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Enter a positive number ");
+            string input = Console.ReadLine();
+            int n = 0;
+            while (!int.TryParse(input, out n) || n < 0 || n > 100)
+            {
+                Console.WriteLine("You haven't entered a correct positive number");
+                input = Console.ReadLine();
+            }
+
+            //int n = 7;
+
+            int[,] matrix = new int[n, n];
+            int stepCount = 0,
+                row = 0,
+                col = 0,
+                rowDirection = 1,
+                colDirection = 1;
+
+            while (FindFirstFreeCell(matrix, ref row, ref col))
+            {
+                stepCount++;
+                rowDirection = 1;
+                colDirection = 1;
+                matrix[row, col] = stepCount;
+
+                while (CheckIfFreeNeighbourCellExists(matrix, row, col))
+                {
+                    if (row + rowDirection >= n || row + rowDirection < 0 || col + colDirection >= n || col + colDirection < 0 || matrix[row + rowDirection, col + colDirection] != 0)
+                    {
+                        while ((row + rowDirection >= n || row + rowDirection < 0 || col + colDirection >= n || col + colDirection < 0 || matrix[row + rowDirection, col + colDirection] != 0))
+                        {
+                            ChangeDirectionClockwise(ref rowDirection, ref colDirection);
+                        }
+                    }
+
+                    row += rowDirection;
+                    col += colDirection;
+                    stepCount++;
+                    matrix[row, col] = stepCount;
+                }
+            }
+
+            PrintMatrix(matrix);
+        }
+
+        public static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(string.Format("{0,5}", matrix[i, j]));
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
