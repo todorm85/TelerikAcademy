3d01e3e1c4ae6a309ef5b52579ba3b51af6b12a2
todorm85 <todorbm85@gmail.com>
2015-10-27 05:54:03 +0200

    Add db exam

---
+++ Software Technologies/Databases/exam check/Problem 5 6 7 - Code First/SocialNetwork.ConsoleClient/Startup.cs
@@ -0,0 +1,25 @@
+﻿namespace SocialNetwork.ConsoleClient
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using Searcher;
+    using SocialNetwork.ConsoleClient.Generator;
+    using SocialNetwork.Data;
+    using SocialNetwork.Data.Migrations;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<SocialNetworkDbContext, Configuration>());
+
+            SocialNetworkDbContext db = new SocialNetworkDbContext();
+            db.Database.CreateIfNotExists();
+
+            //Importer.Create(Console.Out).Import();
+
+            //DataSearcher.Search(new SocialNetworkService());
+        }
+    }
+}
