38fa51f84386d5785f42f945fa489ed0ba3392cd
todorm85 <todorbm85@gmail.com>
2015-11-04 14:21:35 +0200

    Add DSA trees HW

---
+++ Programming/Data-Structures-and-Algorithms/Homework/03. Trees-and-Traversals/03-Directory-tree/DirectoryTree/DirectoryTree.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using MyIO;
+
+namespace DirectoryTree
+{
+    public class DirectoryTree
+    {
+        private DirectoryTree()
+        {
+        }
+
+        public MyDir RootFolder { get; set; }
+
+        public static DirectoryTree CreateTree(string rootPath)
+        {
+            if (Uri.IsWellFormedUriString(rootPath, UriKind.Absolute))
+            {
+                throw new ArgumentException("Invalid root dir path");
+            }
+
+            if (Directory.Exists(rootPath))
+            {
+                throw new ArgumentException("Invalid path");
+            }
+
+            var tree = new DirectoryTree();
+            tree.RootFolder = new MyDir(Path.GetFileName(rootPath));
+
+            GenerateTree(tree.RootFolder, rootPath);
+
+            return tree;
+        }
+
+        private static void GenerateTree(MyDir currentDir, string currentPath)
+        {
+            var filePaths = Directory.GetFiles(currentPath);
+            var files = new List<MyFile>();
+            foreach (var filePath in filePaths)
+            {
+                var currentFile = new FileInfo(filePath);
+                var fileName = currentFile.Name;
+                var fileSize = currentFile.Length;
+
+                var myFile = new MyFile(fileName, fileSize);
+                files.Add(myFile);
+            }
+            currentDir.Files = files.ToArray();
+
+            var dirPaths = Directory.GetDirectories(currentPath);
+
+            var subDirs = new List<MyDir>();
+            foreach (var dirPath in dirPaths)
+            {
+                var currentSubDir = new MyDir(Path.GetDirectoryName(dirPath));
+                GenerateTree(currentSubDir, dirPath);
+                subDirs.Add(currentSubDir);
+            }
+
+            currentDir.Dirs = subDirs.ToArray();
+        }
+    }
+}
---
+++ Programming/Data-Structures-and-Algorithms/Homework/03. Trees-and-Traversals/03-Directory-tree/MyIO/MyDir.cs
@@ -0,0 +1,64 @@
+﻿namespace MyIO
+{
+    public class MyDir
+    {
+        public MyDir(string name)
+        {
+            this.Name = name;
+        }
+
+        public MyFile[] Files { get; set; }
+
+        public MyDir[] Dirs { get; set; }
+
+        public string Name { get; private set; }
+
+        public long CalculateTotalSize()
+        {
+            long totalSize = 0;
+
+            var dirsSize = this.CalculateSubDirsSize();
+            totalSize += dirsSize;
+
+            var filesSize = this.CalculateFilesSize();
+            totalSize += filesSize;
+
+            return totalSize;
+        }
+
+        private long CalculateFilesSize()
+        {
+            long totalSize = 0;
+
+            if (this.Files == null)
+            {
+                return 0;
+            }
+
+            foreach (var file in this.Files)
+            {
+                var fileSize = file.Size;
+                totalSize += fileSize;
+            }
+
+            return totalSize;
+        }
+
+        private long CalculateSubDirsSize()
+        {
+            long totalSize = 0;
+            if (this.Dirs == null)
+            {
+                return 0;
+            }
+
+            foreach (var subDir in this.Dirs)
+            {
+                var subDirSize = subDir.CalculateTotalSize();
+                totalSize += subDirSize;
+            }
+
+            return totalSize;
+        }
+    }
+}
