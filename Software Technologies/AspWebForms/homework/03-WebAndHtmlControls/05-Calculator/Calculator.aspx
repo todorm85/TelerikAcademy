e3decd35eddb4d22326f8a5b5feb7c5fcbd10605
todorm85 <todorbm85@gmail.com>
2016-01-19 23:40:39 +0200

    WebForms: Finish Web and Html Controls homework

---
+++ Software Technologies/AspWebForms/homework/03-WebAndHtmlControls/05-Calculator/Calculator.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="_05_Calculator.Calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        .calc-container {
+            display: inline-block;
+            border: 3px solid black;
+        }
+        .calc-control {
+            margin: 2px;
+            padding: 5px;
+            border: 1px solid black;
+            text-align: center;
+        }
+        .calc-main button {
+            margin: 2px;
+            width: 30px;
+        }
+        .calc-result input {
+            border: 1px solid black;
+            min-width: 50px;
+            text-align: right;
+            padding: 3px;
+        }
+        .calc-calculate {
+            text-align: center;
+        }
+        .calc-calculate button {
+            min-width: 80px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="calc-container">
+            <div class="calc-control calc-result">
+                <input type="text" name="result" value="" runat="server" id="result" disabled="disabled"/>
+            </div>
+            <div class="calc-control calc-main">
+                <button runat="server" onserverclick="Digit_ServerClick">1</button>
+                <button runat="server" onserverclick="Digit_ServerClick">2</button>
+                <button runat="server" onserverclick="Digit_ServerClick">3</button>
+                <button runat="server" onserverclick="Operation_ServerClick">+</button>
+                <br />
+                <button runat="server" onserverclick="Digit_ServerClick">4</button>
+                <button runat="server" onserverclick="Digit_ServerClick">5</button>
+                <button runat="server" onserverclick="Digit_ServerClick">6</button>
+                <button runat="server" onserverclick="Operation_ServerClick">-</button>
+                <br />
+                <button runat="server" onserverclick="Digit_ServerClick">7</button>
+                <button runat="server" onserverclick="Digit_ServerClick">8</button>
+                <button runat="server" onserverclick="Digit_ServerClick">9</button>
+                <button runat="server" onserverclick="Operation_ServerClick">x</button>
+                <br />
+                <button runat="server" onserverclick="Clear_ServerClick">CE</button>
+                <button runat="server" onserverclick="Digit_ServerClick">0</button>
+                <button runat="server" onserverclick="Operation_ServerClick">/</button>
+                <button runat="server" onserverclick="Sqrt_ServerClick">&#8730;</button>
+                <br />
+            </div>
+            <div class="calc-control calc-calculate">
+                <button runat="server" onserverclick="Operation_ServerClick">=</button>
+            </div>
+        </div>
+        <input type="hidden" value="" runat="server" id="lastOperation"/>
+        <input type="hidden" value="" runat="server" id="currentResult"/>
+    </form>
+</body>
+</html>
---
+++ Software Technologies/AspWebForms/homework/03-WebAndHtmlControls/05-Calculator/Calculator.aspx.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace _05_Calculator
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        public const string ErrorMessage = "Error";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Digit_ServerClick(object sender, EventArgs e)
+        {
+            if (this.result.Value == ErrorMessage)
+            {
+                this.result.Value = String.Empty;
+            }
+            var digit = ((HtmlButton)sender).InnerText;
+            if (digit == "0" && this.result.Value == String.Empty)
+            {
+                return;
+            }
+
+            this.result.Value += digit;
+        }
+
+        protected void Operation_ServerClick(object sender, EventArgs e)
+        {
+            if (this.result.Value == String.Empty || this.result.Value == ErrorMessage)
+            {
+                this.result.Value = ErrorMessage;
+                this.lastOperation.Value = String.Empty;
+                this.currentResult.Value = String.Empty;
+                return;
+            }
+
+
+            switch (this.lastOperation.Value)
+            {
+                case "+":
+                    Sum();
+                    break;
+                case "-":
+                    Subtract();
+                    break;
+                case "x":
+                    Multiply();
+                    break;
+                case "/":
+                    Divide();
+                    break;
+                case "":
+                    this.currentResult.Value = this.result.Value;
+                    this.result.Value = String.Empty;
+                    break;
+                default:
+                    break;
+            }
+
+            var operation = ((HtmlButton)sender).InnerText;
+            if (operation == "=")
+            {
+                this.lastOperation.Value = String.Empty;
+                this.result.Value = this.currentResult.Value;
+                this.currentResult.Value = String.Empty;
+                return;
+            }
+
+            this.lastOperation.Value = operation;
+        }
+
+        private void Divide()
+        {
+            this.currentResult.Value = (decimal.Parse(this.currentResult.Value) / decimal.Parse(this.result.Value)).ToString();
+            this.result.Value = String.Empty;
+        }
+
+        private void Multiply()
+        {
+            this.currentResult.Value = (decimal.Parse(this.currentResult.Value) * decimal.Parse(this.result.Value)).ToString();
+            this.result.Value = String.Empty;
+        }
+
+        private void Subtract()
+        {
+            this.currentResult.Value = (decimal.Parse(this.currentResult.Value) - decimal.Parse(this.result.Value)).ToString();
+            this.result.Value = String.Empty;
+        }
+
+        private void Sum()
+        {
+            this.currentResult.Value = (decimal.Parse(this.result.Value) + decimal.Parse(this.currentResult.Value)).ToString();
+            this.result.Value = String.Empty;
+        }
+
+        protected void Clear_ServerClick(Object sender, EventArgs e)
+        {
+            this.result.Value = String.Empty;
+            this.currentResult.Value = String.Empty;
+            this.lastOperation.Value = String.Empty;
+        }
+
+        protected void Sqrt_ServerClick(Object sender, EventArgs e)
+        {
+            try
+            {
+                this.result.Value = Math.Sqrt(double.Parse(this.result.Value)).ToString();
+            }
+            catch (Exception)
+            {
+                this.result.Value = ErrorMessage;
+            }
+        }
+    }
+}
